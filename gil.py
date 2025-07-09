3de42ee9ebeae6e5051c6291a0a058c298fa27dd
sd2k5 <s.dutta2k5@gmail.com>
2025-07-09 21:52:22 +0530

    Added history feature

--- gil.py
+++ gil.py
@@ -4,6 +4,7 @@ import os
 import google.generativeai as genai
 import subprocess
 import json
+from datetime import datetime
 
 load_dotenv()
 PROMPT= "You are a linux system assistant, your job is totake a task given by the user," \
@@ -12,7 +13,7 @@ PROMPT= "You are a linux system assistant, your job is totake a task given by th
     "just the raw text. If the task is an error sent by the user, ONLY return a minor explaination explaining the error, not more than one paragraph  .Here is the task given by the user : "
     
 SCRIPTREVIEW=True
-HISTORYPATH=""
+HISTORYPATH=None
 SAVESCRIPT=True
 
 APIKEY=os.getenv("GEMINI_API_KEY")
@@ -26,14 +27,29 @@ model = genai.GenerativeModel(MODEL)
 
 def config_load():
     global SCRIPTREVIEW
+    global HISTORYPATH
+    global SAVESCRIPT
     with open ("./config.json","r") as f:
         if f==None:
             print("Config not found")
             exit(1)
         config = json.load(f)
         SCRIPTREVIEW=config.get("scriptReview",True)
-       
+        HISTORYPATH=config.get("historyStorage",None)
+        SAVESCRIPT=config.get("saveScripts",False)
 
+        if(SAVESCRIPT==True and HISTORYPATH==None):
+            print("Invalid History path")
+            exit(1)
+       
+def script_save(script):
+    timestamp = datetime.now().strftime("%Y-%M-%d_%H-%M-%S")
+    fileName = f"{HISTORYPATH}/{timestamp}.sh"
+    try:
+        with open(fileName,"w") as f:
+            f.write(script)
+    except:
+        print("Error saving the script...")
 
 def script_gen(task):
     compiledPrompt = PROMPT + task
@@ -63,8 +79,11 @@ def main():
         if confirmation.lower() not in ["yes","y"]:
             print("Aborted")
             exit(0)
+
     subprocess.run(response,shell=True,executable="/bin/bash")
     
+    if SAVESCRIPT==True:
+        script_save(response)
 
 
 if __name__ == "__main__":
